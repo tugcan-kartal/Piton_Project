ace387095ec9aec921648805c657cee1523c9535
tugcan-kartal <tugcankartal01@gmail.com>
2023-01-02 23:16:30 +0300

    after authhh

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -4,6 +4,7 @@ import Slider from 'react-slick';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import {GrAdd} from "react-icons/gr";
 import {MdRemove} from "react-icons/md";
+import { toast } from 'react-hot-toast';
 
 function NextButton ({ onClick, className }) {
 	return (
@@ -77,6 +78,39 @@ function ProductsPage() {
 		]
 	};
 
+	const addToFavourite=async(event)=>{
+
+		const {data}=await Axios.post("https://assignment-api.piton.com.tr/api/v1/product/like",{productId: 2},{
+			headers: {
+				"access-token":lastToken
+			}
+		});
+
+		const statusOfAdd=data.status;
+
+		if (statusOfAdd==="Success") {
+			toast.success("Product is added")
+		} else {
+			toast.error("Product is not added")
+		}
+	}
+
+	const removeToFavourite=async(event)=>{
+		const {data}=await Axios.post("https://assignment-api.piton.com.tr/api/v1/product/like",{productId: 2},{
+			headers: {
+				"access-token":lastToken
+			}
+		});
+
+		const statusOfAdd=data.status;
+
+		if (statusOfAdd==="Success") {
+			toast.success("Product is removed")
+		} else {
+			toast.error("Product is not removed")
+		}
+	}
+
   return (
     
       <div className='bg-gray-50 h-[100vh]'>
@@ -89,8 +123,8 @@ function ProductsPage() {
 
 				{allProducts && allProducts.map(product => (
 					<div key={product.id} className="text-center bg-white rounded-large shadow-md relative">
-						<button className='p-2 rounded-full absolute top-0 left-0 text-black bg-gray-200'> <MdRemove /> </button>
-						<button className='p-2 rounded-full absolute top-0 right-2 text-black bg-gray-200'> <GrAdd /> </button>
+						<button onClick={removeToFavourite} className='p-2 rounded-full absolute top-0 left-0 text-black bg-gray-200'> <MdRemove /> </button>
+						<button onClick={addToFavourite} className='p-2 rounded-full absolute top-0 right-2 text-black bg-gray-200'> <GrAdd /> </button>
 						<img className='w-full scale-75' src='https://www.piton.com.tr/images/banner_image.png'/>
 						<h1 className="font-semibold text-lg">{product.name.slice(0,35)}...</h1>
 						<h1 className="font-bold text-2xl text-blue-700">{product.price} ₺</h1>
