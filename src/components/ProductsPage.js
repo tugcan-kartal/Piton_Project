c897c052d8dd0410d818f6a9c3e5faa0a4399dbe
tugcan-kartal <tugcankartal01@gmail.com>
2023-01-03 00:24:54 +0300

    after solution of getting id.

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -40,43 +40,6 @@ function ProductsPage() {
         });
     },[]);
 
-    const settings = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		nextArrow: <NextButton />,
-		prevArrow: <PrevButton />,
-		responsive: [
-			{
-				breakpoint: 1536,
-				settings: {
-					slidesToShow: 2
-				}
-			},
-			{
-				breakpoint: 1280,
-				settings: {
-					slidesToShow: 2
-				}
-			},
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 2
-				}
-			},
-			{
-				breakpoint: 640,
-				settings: {
-					slidesToShow: 1
-				}
-			}
-		]
-	};
 
 	const addToFavourite=async(event)=>{
 
@@ -119,6 +82,44 @@ function ProductsPage() {
 			toast.error("Product is not removed")
 		}
 	}
+
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		nextArrow: <NextButton />,
+		prevArrow: <PrevButton />,
+		responsive: [
+			{
+				breakpoint: 1536,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 1280,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 640,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	};
 
   return (
     
